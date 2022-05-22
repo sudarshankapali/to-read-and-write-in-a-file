9b5510d3ca6ba75afbb32ac4a411385934b18715
sudarshankapali <sudarshankapalisk@gmail.com>
2022-05-22 10:28:01 +0545

    file program

---
+++ file.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+	ofstream fout;
+	string line;
+	fout.open("file.txt");
+	while(fout){
+		getline(cin,line);
+		if(line=="-1")
+		break;
+			fout<<line<<endl;
+	}
+	fout.close();
+	ifstream fin;
+	fin.open("file.txt");
+	while(fin){
+		getline(fin,line);
+		cout<<line<<endl;
+	}
+	fin.close();
+	return 0;
+}
